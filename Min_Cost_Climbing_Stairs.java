49e52ad7b6c7973a53fbd546b12822b8477a2828
prabhavtrivedi <144278765+prabhavtrivedi@users.noreply.github.com>
2025-03-12 16:41:31 +0530

    Create Min_Cost_Climbing_Stairs.java

---
+++ Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,8 @@
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        // Write your code here
+        int n=cost.length,dp[]=new int[n+1];
+        for(int i=2;i<=n;i++) dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        return dp[n];
+    }
+};
